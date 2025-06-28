8a988155a15a84a01521afb36b9db236e538e90f
tienpd2003 <tienpd2003@gmail.com>
2025-06-28 08:33:29 +0700

    change id sheet

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,13 +9,11 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000,
-      DISPLAY: ':99'
+      PORT: 3000
     },
     env_production: {
       NODE_ENV: 'production',
-      PORT: 3000,
-      DISPLAY: ':99'
+      PORT: 3000
     },
     log_file: './logs/combined.log',
     out_file: './logs/out.log',
